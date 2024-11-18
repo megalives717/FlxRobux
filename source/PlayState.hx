6dcd8c040dbd0ce96a353df003626e7776693e28
megalives717 <megalives717@gmail.com>
2024-11-18 12:21:11 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,9 +7,9 @@ class PlayState extends FlxState
 {
 	override public function create():Void
 	{
-		var video:FlxVideo = new FlxVideo();
+		var video:FlxVideo = new FlxVideo('assets/2.mp4/3.mp4/4.mp4/5.mp4/6.mp4/7.mp4/lipsync.mp4');
 		video.onEndReached.add(video.dispose);
-		video.play('assets/1.mp4/2.mp4/3.mp4/4.mp4/5.mp4/6.mp4/7.mp4/lipsync.mp4');
+		video.play('assets/1.mp4');
 
 		super.create();
 	}
